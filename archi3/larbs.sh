1db57d76ff3b4b945eebadc514b4365d74f1277d
Doug Cooper <dougllcooper@gmail.com>
2019-09-15 01:21:18 -0400

    added a lot of packages and changed script to load progs.csv from here.

--- archi3/larbs.sh
+++ archi3/larbs.sh
@@ -16,7 +16,7 @@ esac done
 
 # DEFAULTS:
 [ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git" && repobranch="archi3"
-[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/archi3/progs.csv"
+[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/dougllcooper/LARBS/master/archi3/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
 [ -z "$repobranch" ] && repobranch="master"
 
