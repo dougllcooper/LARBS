4cb1051f7bd27d0f25fdaae13cf05dd6d37b7f69
Doug Cooper <dougllcooper@gmail.com>
2020-03-24 17:12:59 -0400

    minor modification to pathname in larbs.sh

--- archi3/larbs.sh
+++ archi3/larbs.sh
@@ -15,7 +15,7 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
 esac done
 
 [ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/dougllcooper/LARBS/archi3/progs.csv"
+[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/dougllcooper/LARBS/master/archi3/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
 [ -z "$repobranch" ] && repobranch="master"
 
